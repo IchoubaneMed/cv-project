5dd303c1da4685fa20d15a0efbcf588481fd220b
Mohamed ICHOUBANE <mohamedichoubanetata@gmail.com>
2022-08-08 23:03:29 +0200

    Imported the WorkExperience component inside the App.js file

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import ContactInformation from "./components/ContactInformation";
 import "./styles/global.css"
 import './styles/forms.css';
 import SummaryStatement from "./components/SummaryStatement";
+import WorkExperience from "./components/WorkExperience";
 
 function App() {
   return (
     <div>
       <ContactInformation />
       <SummaryStatement />
+      <WorkExperience />
     </div>
   );
 }
