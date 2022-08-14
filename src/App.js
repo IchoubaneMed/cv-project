97b5941f7de6091e2e6f33b737b656379a7aca37
Mohamed ICHOUBANE <mohamedichoubanetata@gmail.com>
2022-08-14 19:22:20 +0200

    Imported the Form component inside the App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,10 @@
-import ContactInformation from "./components/ContactInformation";
+import Form from './components/Form'
 import "./styles/global.css"
-import './styles/forms.css';
-import SummaryStatement from "./components/SummaryStatement";
-import WorkExperience from "./components/WorkExperience";
-import Education from "./components/Education";
-import Skills from "./components/Skills";
-import Certifications from "./components/Certifications";
 
 function App() {
   return (
     <div>
-      <ContactInformation />
-      <SummaryStatement />
-      <WorkExperience />
-      <Education />
-      <Skills />
-      <Certifications />
+      <Form />   
     </div>
   );
 }
