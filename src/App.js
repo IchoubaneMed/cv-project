048939fd80a7d621581b901f4fafe634b989f999
Mohamed ICHOUBANE <mohamedichoubanetata@gmail.com>
2022-08-14 20:09:39 +0200

    Imported the Header component inside the App.js file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import Form from './components/Form'
+import Header from './components/Header'
 import "./styles/global.css"
 
 function App() {
   return (
     <div className="cv-wrapper">
+      <div className="cv-header">
+        <Header />
+      </div>
       <div className="cv-form">
         <Form />
       </div>   
