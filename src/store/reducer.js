25e893f139207be51bf50f3344d6c2b1607e4dbc
Mohamed ICHOUBANE <mohamedichoubanetata@gmail.com>
2022-08-31 19:24:52 +0200

    Created the reducer for the Skills, Educations, Certifications, and WorkExperience components

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,6 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
 
-
 const initialState = {
     fullName: '',
     email: '',
@@ -10,6 +9,25 @@ const initialState = {
     linkedinUrl: '',
 
     statement: '',
+
+    job_title: '',
+    company_name: '',
+    work_location: '',
+    from: '',
+    to: '',
+    tasks: '',
+
+    degree_name: '',
+    university_name: '',
+    from_education: '',
+    to_education: '',
+
+    expert: '',
+    advanced: '',
+    familiar: '',
+
+    title: '',
+    date: '',
 }
 
 const FULLNAME = 'FULLNAME';
@@ -21,8 +39,27 @@ const LINKEDINURL = 'LINKEDINURL';
 
 const STATEMENT = 'STATEMENT';
 
+const JOBTITLE = 'JOBTITLE';
+const COMPANYNAME = 'COMPANYNAME';
+const WORKLOCATION = 'WORKLOCATION';
+const FROM = 'FROM';
+const TO = 'TO';
+const TASKS = 'TASKS';
+
+const DEGREENAME = 'DEGREENAME';
+const UNIVERSITYNAME = 'UNIVERSITYNAME';
+const FROMEDUCATION = 'FROMEDUCATION';
+const TOEDUCATION = 'TOEDUCATION';
+
+const EXPERT = 'EXPERT';
+const ADVANCED = 'ADVANCED';
+const FAMILIER = 'FAMILIER';
+
+const TITLE = 'TITLE';
+const DATE = 'DATE';
+
 const contactInformationReducer = (state = initialState, action) => {
-    switch(action.type) {
+    switch (action.type) {
         case FULLNAME:
             return {
                 ...state,
@@ -51,15 +88,15 @@ const contactInformationReducer = (state = initialState, action) => {
         case LINKEDINURL:
             return {
                 ...state,
-                linkedinUrl: action.linkedinUrl, 
+                linkedinUrl: action.linkedinUrl,
             }
         default:
-            return state;   
+            return state;
     }
 }
 
 const summaryStatementReducer = (state = initialState, action) => {
-    switch(action.type) {
+    switch (action.type) {
         case STATEMENT:
             return {
                 ...state,
@@ -70,8 +107,111 @@ const summaryStatementReducer = (state = initialState, action) => {
     }
 }
 
+const workExperienceReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case JOBTITLE:
+            return {
+                ...state,
+                job_title: action.job_title,
+            }
+        case COMPANYNAME:
+            return {
+                ...state,
+                company_name: action.company_name,
+            }
+        case WORKLOCATION:
+            return {
+                ...state,
+                work_location: action.work_location,
+            }
+        case FROM:
+            return {
+                ...state,
+                from: action.from,
+            }
+        case TO:
+            return {
+                ...state,
+                to: action.to,
+            }
+        case TASKS:
+            return {
+                ...state,
+                tasks: action.tasks,
+            }
+        default:
+            return state;
+    }
+}
+
+const educationReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case DEGREENAME:
+            return {
+                ...state,
+                degree_name: action.degree,
+            }
+        case UNIVERSITYNAME:
+            return {
+                ...state,
+                university_name: action.university,
+            }
+        case FROMEDUCATION:
+            return {
+                ...state,
+                from_education: action.from_education,
+            }
+        case TOEDUCATION:
+            return {
+                ...state,
+                to_education: action.to_education,
+            }
+        default:
+            return state;
+    }
+}
+
+const skillsReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case EXPERT:
+            return {
+                ...state,
+                expert: action.expert,
+            }
+        case ADVANCED:
+            return {
+                ...state,
+                advanced: action.advanced,
+            }
+        case FAMILIER:
+            return {
+                ...state,
+                familiar: action.familiar,
+            }
+        default:
+            return state;
+    }
+}
+
+const certificationsReducer = (state = initialState, action) => {
+    switch(action.type) {
+        case TITLE:
+            return {
+                ...state,
+                title: action.title,
+            }
+        case DATE:
+            return {
+                ...state,
+                date: action.date,
+            }
+        default:
+            return state;
+    }
+}
+
 const store = configureStore({
-    reducer: {contactInformationReducer, summaryStatementReducer},
+    reducer: { contactInformationReducer, summaryStatementReducer, workExperienceReducer, educationReducer, skillsReducer, certificationsReducer },
 })
 
 export default store;
