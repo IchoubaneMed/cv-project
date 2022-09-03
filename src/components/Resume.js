decdea5291fc075948a92220983d40ae789600e2
Mohamed ICHOUBANE <mohamedichoubanetata@gmail.com>
2022-09-03 14:57:39 +0200

    Added WorkExperience section to Resume.js file

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -5,8 +5,38 @@ import '../styles/resume.css';
 class Resume extends Component {
     constructor(props) {
         super(props)
+        this.handleDate = this.handleDate.bind(this);
     }
 
+    handleDate(date = '') {
+        const months = {
+            Jan: 'January',
+            Feb: 'February',
+            Mar: 'March',
+            Apr: 'April',
+            May: 'May',
+            Jun: 'June',
+            Jul: 'July',
+            Aug: 'August',
+            Sep: 'September',
+            Oct: 'October',
+            Nov: 'November',
+            Dec: 'December',
+        }
+        const currentDate = new Date();
+        const currentMonth = months[currentDate.toString().split(" ")[1]]
+        const currentYear = currentDate.toString().split(" ")[3]
+        
+        const userDate = date.split(" ");
+
+        if(months[userDate[1]] === currentMonth && userDate[3] === currentYear) {
+            return "Present";
+        } else if (date === '') {
+            return "";
+        } else {
+            return `${months[userDate[1]]} ${userDate[3]}`;
+        }
+    }
 
     render() {
         const portfolio = this.props.website.split("").splice(8).join("");
@@ -36,10 +66,17 @@ class Resume extends Component {
                 </div>
 
                 <div className="resume-summaryState">
-                    <h3>Summary Statement</h3>
+                    <h3 className="underline">Summary Statement</h3>
                     <p>{this.props.statement}</p>
                 </div>
 
+                <div className="resume-workExperience">
+                    <h3 className="underline">Work Experience</h3>
+                    <h4 className="inline bold">{this.props.job_title}</h4>, <h4 className="inline bold">{this.props.work_location}</h4>
+                    <h4>{this.props.company_name}</h4>
+                    <h4 className="inline italic">{this.handleDate(this.props.from)}</h4>-<h4 className="inline italic">{this.handleDate(this.props.to)}</h4>
+                    <p>{this.props.tasks}</p>
+                </div>
             </div>
         );
     }
@@ -55,6 +92,13 @@ const mapStateToProps = (state) => {
         linkedinUrl: state.contactInformationReducer.linkedinUrl,
 
         statement: state.summaryStatementReducer.statement,
+
+        job_title: state.workExperienceReducer.job_title,
+        company_name: state.workExperienceReducer.company_name,
+        work_location: state.workExperienceReducer.work_location,
+        from: state.workExperienceReducer.from,
+        to: state.workExperienceReducer.to,
+        tasks: state.workExperienceReducer.tasks,
     }
 }
 
