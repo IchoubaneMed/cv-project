27896da883a2f4a9358cb0afd590f59ccf6e5741
Mohamed ICHOUBANE <mohamedichoubanetata@gmail.com>
2022-09-27 21:30:02 +0200

    Created data.js file

---
+++ src/store/data.js
@@ -0,0 +1,31 @@
+export const data = {
+    fullName: 'Lorem Ipsum',
+    email: 'Lorem_Ipsum@gmail.com',
+    phoneNumber: '+16102458210',
+    website: '',
+    githubUrl: 'https://github.com/LoremIpsum',
+    linkedinUrl: 'https://www.linkedin.com/in/LoremIpsum/',
+
+    statement: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+
+    work_experience: [
+        {id: "l8hr6kl3", job_title: "FrontEnd", work_location: "Mountain View, California", company_name: "Google", from: "Sat Jan 01 2022 21:47:57 GMT+0100 (heure normale d’Europe centrale)", tasks: "", to: "Tue Feb 01 2022 21:47:57 GMT+0100 (heure normale d’Europe centrale)"},
+        {id: "l8hrb0dx", job_title: "FrontEnd", work_location: "Redmond, Washington", company_name: "Microsoft", from: "Fri Apr 01 2022 21:48:12 GMT+0200 (heure d’été d’Europe centrale)", tasks: "", to: "Wed Jun 01 2022 21:48:12 GMT+0200 (heure d’été d’Europe centrale)"}
+    ],
+
+    education: [
+        {degree_name: "Master degree in GIS", from: "Sat Jan 01 2022 21:54:13 GMT+0100 (heure normale d’Europe centrale)", id: "l8hrekxs", to: "Tue Mar 01 2022 21:54:13 GMT+0100 (heure normale d’Europe centrale)", university_name: "Hassan II Ben M'sik Casablanca"},
+        {degree_name: "Bachelor Degree", from: "Fri Apr 01 2022 21:54:23 GMT+0200 (heure d’été d’Europe centrale)", id: "l8hrg8vl", to: "Wed Jun 01 2022 21:54:23 GMT+0200 (heure d’été d’Europe centrale)", university_name: "Faculty of Science of Beni Mellal"}
+    ],
+
+    expert: 'Java, Python, C++, React, JavaScript, SQL, Git',
+    advanced: 'NodeJS, Rust, HTML5, CSS3, Android',
+    familiar: 'Ruby, Web Design, Swift',
+
+    certifications: [
+        { id: "l8hr2dsg", title: "freecodecamp", date: "Sat Jan 01 2022 21:44:46 GMT+0100 (heure normale d’Europe centrale)" },
+        { id: "l8hr3r4d", title: "edx", date: "Tue Feb 01 2022 21:45:10 GMT+0100 (heure normale d’Europe centrale)" },
+        { id: "l8hr48gq", title: "coursera", date: "Tue Mar 01 2022 21:45:56 GMT+0100 (heure normale d’Europe centrale)" }
+    ],
+};
+
